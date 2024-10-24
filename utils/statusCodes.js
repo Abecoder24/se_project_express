44c9b63ce50d3f7f1287f6905aa457a584d966ac
Abecoder24 <103280981+Abecoder24@users.noreply.github.com>
2024-10-24 11:42:14 +0530

    Update statusCodes.js

--- utils/statusCodes.js
+++ utils/statusCodes.js
@@ -3,7 +3,9 @@ const statusCreated = 201;
 const statusNoContent = 204;
 
 const errorBadRequest = 400;
+const errorUnauthorized = 401;
+const errorForbidden = 403;
 const errorNotFound = 404;
 const errorInternalServer = 500;
 
-module.exports = { statusOk, statusCreated, statusNoContent, errorBadRequest, errorNotFound, errorInternalServer };
+module.exports = { statusOk, statusCreated, statusNoContent, errorBadRequest, errorNotFound, errorInternalServer, errorForbidden, errorUnauthorized };
